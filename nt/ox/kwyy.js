c3afd6985aa7df63fc6c7b97781fa7ef218e7847
urnuts <51977940+urnuts@users.noreply.github.com>
2022-01-28 10:32:12 +0800

    Update kwyy.js

--- nt/ox/kwyy.js
+++ nt/ox/kwyy.js
@@ -9,7 +9,7 @@
         请勿转载与贩卖！⚠️⚠️⚠️
 [rewrite_local]
 # 解锁酷我音乐会员➕听书
-^http://(.+).kuwo.cn(/v2/api/user/info|/a.p|/vip/v2/user/vip) url script-response-body https://raw.githubusercontent.com/nameking77/Qx/main/rewrite/kwyy.js
+^http://(.+).kuwo.cn(/v2/api/user/info|/a.p|/vip/v2/user/vip) url script-response-body https://raw.githubusercontent.com/urnuts/haproxy/main/nt/ox/kwyy.js
 [mitm] 
 hostname = *.kuwo.cn
 ********************************
