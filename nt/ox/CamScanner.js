0ee2c99479a4dc1d292c46d4d6812adb94086d59
urnuts <51977940+urnuts@users.noreply.github.com>
2022-01-31 22:43:40 +0800

    Update CamScanner.js

--- nt/ox/CamScanner.js
+++ nt/ox/CamScanner.js
@@ -7,16 +7,15 @@ Quantumult X:
 
 [rewrite_local]
 ^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/urnuts/haproxy/main/nt/ox/CamScanner.js
-
 [mitm]
 hostname = ap*.intsig.net
+
 
 ***************************
 Surge4 or Loon:
 
 [Script]
 http-response ^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/urnuts/haproxy/main/nt/ox/CamScanner.js
-
 [mitm]
 hostname = ap*.intsig.net
 
