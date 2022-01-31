693f82d008e8408fe11b83d777c7cf5fe588ffb6
urnuts <51977940+urnuts@users.noreply.github.com>
2022-01-31 22:43:17 +0800

    Update CamScanner.js

--- nt/ox/CamScanner.js
+++ nt/ox/CamScanner.js
@@ -10,6 +10,17 @@ Quantumult X:
 
 [mitm]
 hostname = ap*.intsig.net
+
+***************************
+Surge4 or Loon:
+
+[Script]
+http-response ^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/urnuts/haproxy/main/nt/ox/CamScanner.js
+
+[mitm]
+hostname = ap*.intsig.net
+
+**************************
 */
 let obj = JSON.parse($response.body);
 obj = {"data":{"psnl_vip_property":{"expiry":"1643731200"}}};
